14ae5a8741cd3b1a894afea7b19b5a3b30a4dfe0
Eugene_Mamychkin <eugenemamychkin@gmail.com>
2025-07-08 21:23:20 +0300

    Добавлен application-test.yml указывающий на embeddedKafka вместо внешней. В UserServiceImpl внедрена зависимость Sender и к методам createUser и deleteUser добавлены постройка сообщения и отправка на email. Написан тест для Sender(см. JavaDoc) Написаны KafkaProducerConfig.java, KafkaProducerTestConfig.java, KafkaSender.java и KafkaTopic.java

---
+++ src/main/java/aston_s2_hw4/kafka/KafkaSender.java
@@ -0,0 +1,33 @@
+package aston_s2_hw4.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+/**
+ * Компонент для отправки сообщений в Kafka.
+ */
+@Component
+public class KafkaSender {
+    /**
+     * Внедрение  kafkaTemplate через конструктор.
+     *
+     */
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    @Autowired
+    public KafkaSender(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    /**
+     * Отправляет сообщение в Kafka с помощью KafkaTemplate.
+     *
+     */
+    public void sendMessage(Message<?> message) {
+
+        System.out.println("Sending : " + message.getPayload());
+        System.out.println("--------------------------------");
+
+        kafkaTemplate.send(message);
+    }
+}
